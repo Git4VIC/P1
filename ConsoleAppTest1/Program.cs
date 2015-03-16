b77a2a4af925872adde857b2580fc48aafcffd3c
Git4VIC <ldai@qcti.net>
2015-03-16 13:18:25 -0400

    For the Win!

---
+++ ConsoleAppTest1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strDT = string.Empty;
+
+            Console.WriteLine("Hello, World!");
+
+            strDT = GetCurrentDateTime();
+            Console.WriteLine("Current DateTime is {0}", strDT);
+
+            Console.ReadKey();
+        }
+
+        static string GetCurrentDateTime()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss, dddd");
+        }
+    }
+}
